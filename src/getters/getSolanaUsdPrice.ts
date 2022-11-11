ea4fdeba07a59dcc6c45d33920aaa09c64f701fd
rustairov <rustairov@gmail.com>
2022-11-11 08:33:55 +0000

    USD price fix

--- src/getters/getSolanaUsdPrice.ts
+++ src/getters/getSolanaUsdPrice.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getSolanaUsdPrice() : Promise<number> {
     try {
-        const priceString = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDC');
+        const priceString = await axios.get('https://api.binance.com/api/v3/ticker/price?symbol=SOLUSDT');
         return Math.floor(parseFloat(priceString.data.price as string) * 100) / 100;
     } catch (err) {
         return 33.5;
